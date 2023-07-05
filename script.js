53a1c0ac36cba453e6b58c904c2eec5356cfc4f0
mthsgrc <matheus.dev.garcia@gmail.com>
2023-07-05 11:02:53 -0300

    Replace cards for Thoth Tarot

--- script.js
+++ script.js
@@ -13,45 +13,41 @@ const card1 = document.querySelector("#card-1");
 const card2 = document.querySelector("#card-2");
 const card3 = document.querySelector("#card-3");
 
+const MIN_CARDS = 0;
+const MAX_CARDS = 77;
+
 
 
 let pickedCards = [];
 
-btnCall.addEventListener('click', () => {
-    let randomResult = 0;
+btnCall.addEventListener('click', () => 
+{
+    // let randomResult = 0;
     pullCard();
-    setTimeout(() => document.getElementById("card-1").innerHTML = "<img src='images/"+pickedCards[0]+".jpg' class='card-img-top' alt='card text'>", 2500);
-    setTimeout(() => document.getElementById("card-2").innerHTML = "<img src='images/"+pickedCards[1]+".jpg' class='card-img-top' alt='card text'>", 3000);
-    setTimeout(() => document.getElementById("card-3").innerHTML = "<img src='images/"+pickedCards[2]+".jpg' class='card-img-top' alt='card text'>", 3500);
+    setTimeout(() => document.getElementById("card-1").innerHTML = "<img src='images/"+pickedCards[0]+".png' class='card-img-top' alt='card text'>", 2500);
+    setTimeout(() => document.getElementById("card-2").innerHTML = "<img src='images/"+pickedCards[1]+".png' class='card-img-top' alt='card text'>", 3500);
+    setTimeout(() => document.getElementById("card-3").innerHTML = "<img src='images/"+pickedCards[2]+".png' class='card-img-top' alt='card text'>", 4500);
 
-    setTimeout(() => document.getElementById("change-btn").innerHTML = "<button type=\"button\" onClick=\"window.location.reload();\" class=\"btn btn-info btn-lg btn-block\">New Reading</button>", 3500);
+    setTimeout(() => document.getElementById("change-btn").innerHTML = "<button type=\"button\" onClick=\"window.location.reload();\" class=\"btn btn-info btn-lg btn-block\">New Reading</button>", 4500);
 });
 
 
-function pullCard() {
-    let cardNumber = getRandomArbitrary(0, 156);
+function pullCard() 
+{
+
+    let cardNumber = getRandomArbitrary(MIN_CARDS, MAX_CARDS);
 }
 
-function getRandomArbitrary(min, max) {
-
-    while (pickedCards.length < 3) {
+function getRandomArbitrary(min, max) 
+{
+    while (pickedCards.length < 3) 
+    {
         result = Math.floor(Math.random() * (max - min) + min);
 
-        if ((pickedCards.indexOf(result) == -1) && 
-            (pickedCards.includes(result + 78) != true) &&
-            (pickedCards.includes(result - 78) != true)) {
-        
+        if (pickedCards.indexOf(result) == -1) 
+        {
             pickedCards.push(result);
         }
     }
 }
 
-
-
-// test
-// function getRandomArbitrary2(min, max) {
-//     for (var i = 0; i < 20; i++) {
-//         result = Math.random() * (max - min) + min;
-//         console.log(result.toFixed(0));
-//     }
-// }
